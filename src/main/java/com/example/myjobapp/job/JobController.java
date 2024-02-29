eb31d9602de85634ecbe9aea9fb23ceac4d36af4
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-29 11:41:14 +0100

    job repo

--- src/main/java/com/example/myjobapp/job/JobController.java
+++ src/main/java/com/example/myjobapp/job/JobController.java
@@ -1,9 +1,11 @@
 package com.example.myjobapp.job;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JobController {
+    private List <Job> jobs = new ArrayList<>();
     public List<Job>findAll(){
-        return job
+        return jobs;
     }
 }
