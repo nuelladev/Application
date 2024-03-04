63939098d6854f46dab916a7135840afcc0d735a
nuelladev <emmanuellaokafor42@yahoo.com>
2024-03-05 00:10:43 +0100

    Company repo

--- src/main/java/com/example/myjobapp/company/Company.java
+++ src/main/java/com/example/myjobapp/company/Company.java
@@ -1,4 +1,23 @@
 package com.example.myjobapp.company;
 
+import com.example.myjobapp.job.Job;
+import jakarta.persistence.*;
+
+import java.util.List;
+@Entity
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String companyName;
+    private String description;
+    @OneToMany
+    private List<Job> jobs;
+
+    public Company(Long id, String companyName, String description, List<Job> jobs) {
+        this.id = id;
+        this.companyName = companyName;
+        this.description = description;
+        this.jobs = jobs;
+    }
 }
